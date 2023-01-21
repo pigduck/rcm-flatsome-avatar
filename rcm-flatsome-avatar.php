fc4df27aadbc34c55db50ed67981139e6ca23923
DESKTOP-564I9MQ\black <heiblack850930@gmail.com>
2023-01-21 11:30:40 +0800

    RCM Flatsome Avatar

--- rcm-flatsome-avatar.php
+++ rcm-flatsome-avatar.php
@@ -17,9 +17,6 @@ add_action( 'wp_enqueue_scripts', function () {
         wp_enqueue_script( 'my-account', plugins_url( '/js/my-account.js', __FILE__ ));
     }
 } );
-
-//wp ajax action
-
 add_action('wp_ajax_rcm_flatsome_avatar_upload', function ()
 {
     $file = $_FILES['rcm_eump_image'];
@@ -37,39 +34,24 @@ add_action('wp_ajax_rcm_flatsome_avatar_upload', function ()
 });
 
 add_action( 'woocommerce_account_content', function () {
-
-
-
     echo "<input type=\"file\" id=\"image\" accept=\"image/*\" style=\"display: none\">";
-
     echo "<input type=\"button\" id=\"save_image\" style=\"display: none\">";
-
-
-
 });
 
-
-
-
 add_filter( 'get_avatar', function ( $avatar, $id_or_email, $size, $default, $alt ) {
-
     if ( ! is_numeric( $id_or_email ) && is_email( $id_or_email->comment_author_email ) ) {
         $user = get_user_by( 'email', $id_or_email );
         if ( $user ) {
             $id_or_email = $user->ID;
         }
     }
-
     if( ! is_numeric( $id_or_email ) ) {
         return $avatar;
     }
-
     $attachment_id  = get_user_meta( $id_or_email, 'image', true );
-
     if ( ! empty ( $attachment_id  ) ) {
         return wp_get_attachment_image( $attachment_id, [ $size, $size ], false, ['alt' => $alt] );
     }
-
     return $avatar;
 }, 10, 5 );
 
